e85bfc37f460b658d62473ca20bff6a93620e619
Matías Barrios <matias.barriosn@gmail.com>
2021-12-08 17:12:28 -0300

    feat: tailwinds installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        monoton: ['Monoton', 'cursive'],
+      },
+      textColor: {
+        yellow: '#ecd30f',
+      },
+    },
   },
   variants: {
     extend: {},
